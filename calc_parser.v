c5035c169a71502657b592c0b63277fdf239f0c6
fermarsan <fmartinezsanta@gmail.com>
2025-02-19 09:50:34 -0500

    add example file

---
+++ calc_parser.v
@@ -0,0 +1,86 @@
+// Parser example
+import os
+import vly { 
+    parser 
+    parser.prod    // production
+}
+
+// Get the token map from the lexer.  This is required.
+import calc_lexer { tokens }
+
+p_expression_plus := GrammarRule {
+    rule:   'expression : expression PLUS term'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1] + p[3]
+    }
+}
+
+p_expression_minus := GrammarRule {
+    rule:   'expression : expression MINUS term'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1] - p[3]
+    }
+}
+
+p_expression_term := GrammarRule {
+    rule:   'expression : term'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1]
+    }
+}
+
+p_term_times := GrammarRule {
+    rule:   'term : term TIMES factor'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1] * p[3]
+    }
+}
+
+p_term_div := GrammarRule {
+    rule:   'term : term DIVIDE factor'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1] / p[3]
+    }
+}
+
+p_term_factor := GrammarRule {
+    rule:   'term : factor'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1]
+    }
+}
+
+p_factor_num := GrammarRule {
+    rule:   'factor : NUMBER'
+    impl:   fn (p prod) prod {
+        return p[0] = p[1]
+    }
+}
+
+p_factor_expr := GrammarRule {
+    rule:   'factor : LPAREN expression RPAREN'
+    impl:   fn (p prod) prod {
+        return p[0] = p[2]
+    }
+}
+
+// Error rule for syntax errors
+fn p_error(p prod) {
+    println('Syntax error in input!')
+}
+    
+
+// Build the parser
+parser := parser.parser()
+
+for {
+    s := input('calc > ') or {
+        break
+    }
+    if !s { continue }
+    result := parser.parse(s)
+    println(result)
+}
+
+// This file was inspired by the PLY calc example 
+// https://ply.readthedocs.io/en/latest/ply.html#an-example 

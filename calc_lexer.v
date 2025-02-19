27d7789d7a3937980d6d2ff8931d0b03d0a14b01
fermarsan <fmartinezsanta@gmail.com>
2025-02-19 10:10:04 -0500

    add lexer example file

---
+++ calc_lexer.v
@@ -0,0 +1,61 @@
+module calc_lexer
+
+import vly {
+	lexer
+	token
+}
+
+// List of token names.   This is always required
+tokens := [
+   'number',
+   'plus',
+   'minus',
+   'times',
+   'divide',
+   'lparen',
+   'rparen',
+]
+
+// Regular expression rules for simple tokens
+t_plus    := '\+'
+t_minus   := '-'
+t_times   := '\*'
+t_divide  := '/'
+t_lparen  := '\('
+t_rparen  := '\)'
+
+// A regular expression rule with some action code
+t_number := rule.TokenRule {
+    rule:   '\d+'
+    impl:   fn (t token) token {
+		t.value = int(t.value)
+		return t
+    }
+}
+
+// Define a rule so we can track line numbers
+t_newline := rule.TokenRule {
+    rule:   '\n+'
+    impl:   fn (t token) token {
+		t.lexer.lineno += len(t.value)
+		return nil
+    }
+}
+
+// A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+// Error handling rule
+fn t_error(t token):
+    println("Illegal character '${t.value[0]}'")
+    t.lexer.skip(1)
+
+// Build the lexer
+lexer = lexer.lexer()
+
+
+// struct LexerRule {
+// mut:
+// 	rule string
+// 	impl fn(p vly.lexer.token) vly.lexer.token
+// }
--- calc_parser.v
+++ calc_parser.v
@@ -1,6 +1,7 @@
 // Parser example
 import os
 import vly { 
+	rule
     parser 
     parser.prod    // production
 }
@@ -8,56 +9,56 @@ import vly {
 // Get the token map from the lexer.  This is required.
 import calc_lexer { tokens }
 
-p_expression_plus := GrammarRule {
+p_expression_plus := rule.GrammarRule {
     rule:   'expression : expression PLUS term'
     impl:   fn (p prod) prod {
         return p[0] = p[1] + p[3]
     }
 }
 
-p_expression_minus := GrammarRule {
+p_expression_minus := rule.GrammarRule {
     rule:   'expression : expression MINUS term'
     impl:   fn (p prod) prod {
         return p[0] = p[1] - p[3]
     }
 }
 
-p_expression_term := GrammarRule {
+p_expression_term := rule.GrammarRule {
     rule:   'expression : term'
     impl:   fn (p prod) prod {
         return p[0] = p[1]
     }
 }
 
-p_term_times := GrammarRule {
+p_term_times := rule.GrammarRule {
     rule:   'term : term TIMES factor'
     impl:   fn (p prod) prod {
         return p[0] = p[1] * p[3]
     }
 }
 
-p_term_div := GrammarRule {
+p_term_div := rule.GrammarRule {
     rule:   'term : term DIVIDE factor'
     impl:   fn (p prod) prod {
         return p[0] = p[1] / p[3]
     }
 }
 
-p_term_factor := GrammarRule {
+p_term_factor := rule.GrammarRule {
     rule:   'term : factor'
     impl:   fn (p prod) prod {
         return p[0] = p[1]
     }
 }
 
-p_factor_num := GrammarRule {
+p_factor_num := rule.GrammarRule {
     rule:   'factor : NUMBER'
     impl:   fn (p prod) prod {
         return p[0] = p[1]
     }
 }
 
-p_factor_expr := GrammarRule {
+p_factor_expr := rule.GrammarRule {
     rule:   'factor : LPAREN expression RPAREN'
     impl:   fn (p prod) prod {
         return p[0] = p[2]
